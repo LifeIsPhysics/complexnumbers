06f87b74969c6d6153e00b004a0232d3f5e8c9a5
Berke Mert <117230395+LifeIsPhysics@users.noreply.github.com>
2023-06-07 01:10:47 +0200

    Updated main

--- complexnumbers/main.cpp
+++ complexnumbers/main.cpp
@@ -1,16 +1,20 @@
-#include "mycomplex.h"
+#include "my_complex.h"
 
 int main() {
     // TESTS
-    Complex<double> c1{1.2, 2.7};
-    Complex<double> c2{2.67124598765, .1289731824};
+    Complex c1{1., 1.};
+    Complex c2{1., 2.};
 
-    print(add(c1, c2));
-    print(conjugate(c1));
-    print(reciprocal(c2));
-    print(divide(c1, c2));
-    print(subtract(c1, c2));
-    print(multiply(c1, c2));
-    print(squared(c1));
+    std::cout << c1.getReal() << '\n';          // 1
+    std::cout << c2.getImag() << '\n';          // 2
+    c1.add(c2).print();                         // 2+j3
+    c1.subtract(c1).print();                    // 0+j0
+    c1.add(c2).subtract(c2).print();            // 1+j1
+    c1.multiply(c2).print();                    // -1+j3
+    c1.squared().print();                       // 0+j2
+    c1.divide(c2).print();                      // 0.6-j0.2
+    c2.reciprocal().print();                    // 0.2-j0.4
+
+
     return 0;
 }
